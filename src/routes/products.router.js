98dbf6d7015b72128b5fdafae15f96c90b7619f9
sebastianpazfriaz <sebastian.pazfriaz@gmail.com>
2026-06-26 02:06:46 -0300

    Implementación final con WebSockets y vistas y cambios menores

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -61,64 +61,6 @@ router.get('/', async (req, res) => {
         });
 
     }
-});router.get('/', async (req, res) => {
-    try {
-
-        const {
-            limit = 10,
-            page = 1,
-            sort,
-            query
-        } = req.query;
-
-
-        let filter = {};
-
-        if (query) {
-            filter = {
-                category: query
-            };
-        }
-
-
-        let options = {
-            limit,
-            page
-        };
-
-
-        if (sort) {
-            options.sort = {
-                price: sort === 'asc' ? 1 : -1
-            };
-        }
-
-
-        const result = await Product.paginate(filter, options);
-
-
-        res.json({
-            status: 'success',
-            payload: result.docs,
-            totalPages: result.totalPages,
-            prevPage: result.prevPage,
-            nextPage: result.nextPage,
-            page: result.page,
-            hasPrevPage: result.hasPrevPage,
-            hasNextPage: result.hasNextPage,
-            prevLink: null,
-            nextLink: null
-        });
-
-
-    } catch (error) {
-
-        res.status(500).json({
-            status: 'error',
-            message: error.message
-        });
-
-    }
 });
 
 router.get('/:pid', async (req, res) => {
@@ -176,18 +118,17 @@ router.put('/:pid', async (req, res) => {
             { new: true }
         );
 
-
-        req.app.get('io').emit('productUpdate', {
-            action: 'update',
-            product
-        });
-
         if (!product) {
             return res.status(404).json({
                 status: 'error',
                 message: 'Producto no encontrado'
             });
         }
+
+        req.app.get('io').emit('productUpdate', {
+            action: 'update',
+            product
+        });
 
         res.json({
             status: 'success',
@@ -207,17 +148,17 @@ router.delete('/:pid', async (req, res) => {
     try {
         const product = await Product.findByIdAndDelete(req.params.pid);
 
-        req.app.get('io').emit('productUpdate', {
-            action: 'delete',
-            product
-        });
-
         if (!product) {
             return res.status(404).json({
                 status: 'error',
                 message: 'Producto no encontrado'
             });
         }
+
+        req.app.get('io').emit('productUpdate', {
+            action: 'delete',
+            product
+        });
 
         res.json({
             status: 'success',
